cf463af23cb9386607fda919787a4e5de5fe144e
Mario Mann <mario.mann@novatec-gmbh.de>
2021-01-08 14:14:24 +0100

    Source code adapted to typescript convention

--- src/panel/statistics/NodeStatistics.tsx
+++ src/panel/statistics/NodeStatistics.tsx
@@ -1,47 +1,46 @@
 import React from 'react';
 import { TableContent } from 'types';
-import roundPercentageToDecimal from './utils/Utils'
+import roundPercentageToDecimal from './utils/Utils';
 
-interface NodeStatisticsProps  {
-    nodeList: TableContent[],
-    noDataText: string,
-    title: string  
+interface NodeStatisticsProps {
+  nodeList: TableContent[];
+  noDataText: string;
+  title: string;
 }
 
-export const NodeStatistics: React.FC<NodeStatisticsProps> = ({nodeList, noDataText, title}) => {
-        var nodeStatistics = (<div className="no-data--selection">{noDataText}</div>)
-        if(nodeList.length > 0) {
-            var recievingNodes = nodeList.map((node: any) => {
-                
-                return ( 
-                    <tr>
-                        <td className="table--td--selection" title={node.name}>{node.name}</td>
-                        <td className="table--td--selection">{node.responseTime}</td>
-                        <td className="table--td--selection">{node.rate}</td>
-                        <td className="table--td--selection">{roundPercentageToDecimal(2, node.error)}</td>
-                    </tr>
-                )
-            }
-            );
-            
+export const NodeStatistics: React.FC<NodeStatisticsProps> = ({ nodeList, noDataText, title }) => {
+  var nodeStatistics = <div className="no-data--selection">{noDataText}</div>;
+  if (nodeList.length > 0) {
+    var recievingNodes = nodeList.map((node: any) => {
+      return (
+        <tr>
+          <td className="table--td--selection" title={node.name}>
+            {node.name}
+          </td>
+          <td className="table--td--selection">{node.responseTime}</td>
+          <td className="table--td--selection">{node.rate}</td>
+          <td className="table--td--selection">{roundPercentageToDecimal(2, node.error)}</td>
+        </tr>
+      );
+    });
 
-            nodeStatistics = (
-                <table className="table--selection">
-                    <tr className="table--selection--head">
-                        <th>Name</th>
-                        <th className="table--th--selectionSmall">Time</th>
-                        <th className="table--th--selectionSmall">Requests</th>
-                        <th className="table--th--selectionSmall">Error Rate</th>
-                    </tr>
-                    {recievingNodes}
-                </table>
-            );
-        }
-        
-        return (
-            <div>
-                <div className="secondHeader--selection">{title}</div>
-                {nodeStatistics}
-            </div>
-        )
-    }
+    nodeStatistics = (
+      <table className="table--selection">
+        <tr className="table--selection--head">
+          <th>Name</th>
+          <th className="table--th--selectionSmall">Time</th>
+          <th className="table--th--selectionSmall">Requests</th>
+          <th className="table--th--selectionSmall">Error Rate</th>
+        </tr>
+        {recievingNodes}
+      </table>
+    );
+  }
+
+  return (
+    <div>
+      <div className="secondHeader--selection">{title}</div>
+      {nodeStatistics}
+    </div>
+  );
+};
--- src/panel/statistics/Statistics.tsx
+++ src/panel/statistics/Statistics.tsx
@@ -1,132 +1,136 @@
 import React from 'react';
-import { NodeStatistics } from './NodeStatistics'
+import { NodeStatistics } from './NodeStatistics';
 import '../../css/novatec-service-dependency-graph-panel.css';
 import './Statistics.css';
 import { TableContent } from 'types';
 import roundPercentageToDecimal from './utils/Utils';
 
-interface StatisticsProps  {
-    show: boolean,
-    selectionId: string | number, 
-    resolvedDrillDownLink: string, 
-    selectionStatistics: any,
-    currentType: string,
-    showBaselines: boolean,
-    receiving: TableContent[],
-    sending: TableContent[]
+interface StatisticsProps {
+  show: boolean;
+  selectionId: string | number;
+  resolvedDrillDownLink: string;
+  selectionStatistics: any;
+  currentType: string;
+  showBaselines: boolean;
+  receiving: TableContent[];
+  sending: TableContent[];
 }
 
 export const Statistics: React.FC<StatisticsProps> = ({
-    show,
-    selectionId, 
-    resolvedDrillDownLink, 
-    selectionStatistics,
-    currentType,
-    showBaselines,
-    receiving,
-    sending
-    }) => {
-        
-        var statistics=(<div></div>)
-        if(show) {
-            var drilldownLink = (<div></div>)
-            if(resolvedDrillDownLink && resolvedDrillDownLink.length > 0 && currentType === 'INTERNAL'){
-
-                drilldownLink = (
-                    <a target="_blank" href={resolvedDrillDownLink}>
-                        <i className="fa fa-paper-plane-o margin"></i>
-                    </a>
-                    )
-            }
-
-            var requests=(<div></div>)
-            if(selectionStatistics.requests >= 0) {
-                requests = ( 
-                    <tr>
-                        <td className="table--td--selection">Requests</td>
-                        <td className="table--td--selection">{selectionStatistics.requests}</td>
-                    </tr>
-                )
-            }
-
-            var errors=(<div></div>)
-            if(selectionStatistics.errors >= 0) {
-                errors = (
-                    <tr>
-                        <td className="table--td--selection">Errors</td>
-                        <td className="table--td--selection">{selectionStatistics.errors}</td>
-                    </tr>
-                    )
-            }
-
-            var errorRate=(<div></div>)
-            if(selectionStatistics.requests >= 0 && selectionStatistics.errors >= 0){
-                errorRate = (
-                    <tr ng-show="">
-                        <td className="table--td--selection">Error Rate</td>
-                        <td className="table--td--selection">{ roundPercentageToDecimal(2, (100 / selectionStatistics.requests * selectionStatistics.errors))}</td>
-                    </tr>
-                )
-            }
-
-            var avgResponseTime=(<div></div>)
-            if(selectionStatistics.responseTime >= 0) {
-                avgResponseTime = (
-                    <tr>
-                        <td className="table--td--selection">Avg. Response Time</td>
-                        <td className="table--td--selection">{selectionStatistics.responseTime} ms</td>
-                    </tr>
-                )
-            }
-
-            var baseline=(<div></div>)
-            if (showBaselines && selectionStatistics.threshold) {
-
-                var threshold = (<td className="table--td--selection threshold--good">Good "(&lt;= {selectionStatistics.threshold}ms)"</td>)
-                if(selectionStatistics.thresholdViolation) {
-                    threshold = <td className="table--td--selection threshold--bad">Bad ({">"} {selectionStatistics.threshold}ms)</td>
-                }
-                baseline = (
-                    <tr>
-                        <td className="table--td--selection">Response Time Health (Upper Baseline)</td>
-                        {threshold}
-                    </tr>
-                    )
-            }
-            statistics = (
-                <div >
-                    <div className="header--selection">{selectionId}
-                        {drilldownLink}
-                    </div>
-
-                    <div className="secondHeader--selection">Statistics</div>
-                    <table className="table--selection">
-                        <tr className="table--selection--head">
-                            <th>Name</th>
-                            <th className="table--th--selectionMedium">Value</th>
-                        </tr>
-                        {requests}    
-                        {errors}
-                        {errorRate}
-                        {avgResponseTime}
-                        {baseline}
-                    </table>
-
-                    <NodeStatistics nodeList={receiving} noDataText="No incoming statistics available." title="Incoming Statistics"/>
-                    <NodeStatistics nodeList={sending} noDataText = "No outgoing statistics available." title="Outgoing Statistics"/>
-                </div>
-            )
-        }
-        
-        var w = "0%"
-        if(show) {
-            w = "40%"
-        }
-        return (
-            <div style={{width: w}}>
-                {statistics}
-            </div>
-        );
+  show,
+  selectionId,
+  resolvedDrillDownLink,
+  selectionStatistics,
+  currentType,
+  showBaselines,
+  receiving,
+  sending,
+}) => {
+  var statistics = <div></div>;
+  if (show) {
+    var drilldownLink = <div></div>;
+    if (resolvedDrillDownLink && resolvedDrillDownLink.length > 0 && currentType === 'INTERNAL') {
+      drilldownLink = (
+        <a target="_blank" href={resolvedDrillDownLink}>
+          <i className="fa fa-paper-plane-o margin"></i>
+        </a>
+      );
     }
 
+    var requests = <div></div>;
+    if (selectionStatistics.requests >= 0) {
+      requests = (
+        <tr>
+          <td className="table--td--selection">Requests</td>
+          <td className="table--td--selection">{selectionStatistics.requests}</td>
+        </tr>
+      );
+    }
 
+    var errors = <div></div>;
+    if (selectionStatistics.errors >= 0) {
+      errors = (
+        <tr>
+          <td className="table--td--selection">Errors</td>
+          <td className="table--td--selection">{selectionStatistics.errors}</td>
+        </tr>
+      );
+    }
+
+    var errorRate = <div></div>;
+    if (selectionStatistics.requests >= 0 && selectionStatistics.errors >= 0) {
+      errorRate = (
+        <tr ng-show="">
+          <td className="table--td--selection">Error Rate</td>
+          <td className="table--td--selection">
+            {roundPercentageToDecimal(2, (100 / selectionStatistics.requests) * selectionStatistics.errors)}
+          </td>
+        </tr>
+      );
+    }
+
+    var avgResponseTime = <div></div>;
+    if (selectionStatistics.responseTime >= 0) {
+      avgResponseTime = (
+        <tr>
+          <td className="table--td--selection">Avg. Response Time</td>
+          <td className="table--td--selection">{selectionStatistics.responseTime} ms</td>
+        </tr>
+      );
+    }
+
+    var baseline = <div></div>;
+    if (showBaselines && selectionStatistics.threshold) {
+      var threshold = (
+        <td className="table--td--selection threshold--good">Good "(&lt;= {selectionStatistics.threshold}ms)"</td>
+      );
+      if (selectionStatistics.thresholdViolation) {
+        threshold = (
+          <td className="table--td--selection threshold--bad">
+            Bad ({'>'} {selectionStatistics.threshold}ms)
+          </td>
+        );
+      }
+      baseline = (
+        <tr>
+          <td className="table--td--selection">Response Time Health (Upper Baseline)</td>
+          {threshold}
+        </tr>
+      );
+    }
+    statistics = (
+      <div>
+        <div className="header--selection">
+          {selectionId}
+          {drilldownLink}
+        </div>
+
+        <div className="secondHeader--selection">Statistics</div>
+        <table className="table--selection">
+          <tr className="table--selection--head">
+            <th>Name</th>
+            <th className="table--th--selectionMedium">Value</th>
+          </tr>
+          {requests}
+          {errors}
+          {errorRate}
+          {avgResponseTime}
+          {baseline}
+        </table>
+
+        <NodeStatistics
+          nodeList={receiving}
+          noDataText="No incoming statistics available."
+          title="Incoming Statistics"
+        />
+        <NodeStatistics nodeList={sending} noDataText="No outgoing statistics available." title="Outgoing Statistics" />
+      </div>
+    );
+  }
+
+  var w = '0%';
+  if (show) {
+    w = '40%';
+  }
+  return <div style={{ width: w }}>{statistics}</div>;
+};
